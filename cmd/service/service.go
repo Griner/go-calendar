f572a224b7d1db79612f38f7985bfc3ce840385b
Griner <griner9@gmail.com>
2019-08-31 17:07:11 +0600

    init

---
+++ cmd/service/service.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/griner/go-calendar/internal/config"
+	"log"
+)
+
+var configFile string
+
+func init() {
+	flag.StringVar(&configFile, "config", "", "service config")
+}
+
+func main() {
+
+	flag.Parse()
+
+	if len(configFile) == 0 {
+		configFile = "config/config.yml"
+	}
+
+	// load app config
+	cfg, err := config.LoadConfig(configFile)
+	if err != nil {
+		log.Fatalf("Load configuration %s error: %s", configFile, err)
+	}
+
+	// load log config
+	logger, err:= config.GetLogger(&cfg.Logger)
+	if err != nil {
+		log.Fatalf("Logger setup error: %s", err)
+	}
+	defer logger.Sync()
+
+	logger.Info("logger construction succeeded")
+	logger.Debug(fmt.Sprintf("Config %#v\n", cfg))
+
+	// TODO ...
+}
